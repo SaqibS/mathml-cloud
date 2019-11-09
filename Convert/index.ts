1a45f0a6b9c31b946286db475b7507ec83986920
Saqib Shaikh <saqibs@microsoft.com>
2019-11-09 13:55:28 -0800

    Implementing convert function

--- Convert/index.ts
+++ Convert/index.ts
@@ -1,9 +1,10 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import mathjax = require("mathjax-node-sre");
 
 const convert: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('Convert function processed a request.');
 
-    var format = req.query.format;
+    const format = req.query.format;
     if (!format) {
         context.res = {
             status: 400,
@@ -12,8 +13,7 @@ const convert: AzureFunction = async function (context: Context, req: HttpReques
         return;
     }
 
-    format = format.toLowerCase();
-    if (format != "mathml" && format != "asciimath" && format != "latex") {
+    if (format != "MathML" && format != "AsciiMath" && format != "TeX") {
         context.res = {
             status: 400,
             body: "Unsupported format '" + format + "'"
@@ -21,9 +21,17 @@ const convert: AzureFunction = async function (context: Context, req: HttpReques
         return;
     }
 
+    var result = await mathjax.typeset({
+        math: req.body,
+        format: format,
+        mml: true,
+        svg: true
+    });
+
     context.res = {
-        body: "Converting from " + req.query.format
+        body: result.mml
     };
 };
 
 export default convert;
+
